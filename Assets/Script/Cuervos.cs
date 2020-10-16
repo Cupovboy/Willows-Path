d5e3f4a99ad4eaa0ee7e5706be80b05a90c9189f
NataliaHu <71192464+NataliaHu@users.noreply.github.com>
2020-10-15 19:41:06 -0500

    Actualización15OCT
    Se agregó sistema de cuervos visible

---
+++ Assets/Script/Cuervos.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuervos : MonoBehaviour
+{
+    public static int _cCero = 0;
+    public static int _cUno = 0;
+    public static int _cDos = 0;
+    public static int _cTres = 0;
+    public static int _cCuatro = 0;
+
+    internal static object GetObject()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int _cCinco = 0;
+
+    private void Update()
+    {
+        if (_cCero >= 1)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+        if (_cUno >= 1)
+        {
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+
+        if (_cDos >= 1)
+        {
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        if (_cTres >= 1)
+        {
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        if (_cCuatro >= 1)
+        {
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        if (_cCinco >= 1)
+        {
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+
+    }
+
+    public static void GetcCero()
+    {
+        _cCero++;
+    }
+
+    public static void GetcUno()
+    {
+        _cUno++;
+    }
+
+    public static void GetcDos()
+    {
+        _cDos++;
+    }
+
+    public static void GetcTres()
+    {
+        _cTres++;
+    }
+    public static void GetcCuatro()
+    {
+        _cCuatro++;
+    }
+    public static void GetcCinco()
+    {
+        _cCinco++;
+    }
+}
---
+++ Assets/Script/GetCollect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCollect : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            object p = Cuervos.GetObject();
+        }
+    }
+}
